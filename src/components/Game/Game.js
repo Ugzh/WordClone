fd51cf0ab8c8268344df71d5dfe0b63ebc4d9df8
Ugo Thevenin <ugo.mateda@gmail.com>
2024-06-17 09:24:29 +0200

    MAJ input state

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,16 +4,27 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 
 import GuessInput from "../GuessInput/GuessInput";
+import GuessResults from "../GuessResults/GuessResults";
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+export const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSumbitGuess(tentativeGuess) {
+    setGuesses([...guesses, tentativeGuess]);
+  }
   return (
     <>
-      <GuessInput />
+      <GuessResults guesses={guesses} answer={answer} />
+      <GuessInput
+        handleSumbitGuess={handleSumbitGuess}
+        guesses={guesses}
+        answer={answer}
+      />
     </>
   );
 }
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,29 +1,39 @@
 import React from "react";
+import GameOverBanner from "../GameOverBanner/GameOverBanner";
 
-function GuessInput() {
-  const [guess, setGuess] = React.useState("");
+function GuessInput({ handleSumbitGuess, guesses, answer }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState("");
 
   function handleSumbit(event) {
     event.preventDefault();
-    console.log({ guess });
-    setGuess("");
+    handleSumbitGuess(tentativeGuess);
+    setTentativeGuess("");
   }
+
+  const [disabled, setDisabled] = React.useState(false);
+
   return (
     <div>
       <form className="guess-input-wrapper" onSubmit={handleSumbit}>
         <label htmlFor="guess-input">Enter guess:</label>
         <input
           required
+          disabled={disabled}
           type="text"
           id="guess-input"
-          value={guess}
+          value={tentativeGuess}
           pattern="[A-Za-z]{5}"
           title="You must write 5 letters words"
           onChange={(e) => {
-            setGuess(e.target.value.toUpperCase());
+            setTentativeGuess(e.target.value.toUpperCase());
           }}
         ></input>
       </form>
+      <GameOverBanner
+        value={guesses}
+        answer={answer}
+        setDisabled={setDisabled}
+      />
     </div>
   );
 }
